898f8b39344f663a74c0f140ff25a623b42aa4f9
bhoon <leafloveu@naver.com>
2024-09-10 18:43:20 +0900

    FEAT: ãStock,, StockData Entity 병í합

--- src/main/java/com/example/stonks/stock/StockController.java
+++ src/main/java/com/example/stonks/stock/StockController.java
@@ -1,8 +1,5 @@
 package com.example.stonks.stock;
 
-import com.example.stonks.data.StockData;
-import com.example.stonks.data.StockDataRepository;
-import com.example.stonks.data.StockDataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,35 +8,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class StockController {
 
     private final StockRepository stockRepository;
-    private final StockDataRepository stockDataRepository;
+    private final StockService stockService;
 
     // GetMapping ----------------------------------------
 
-    @GetMapping("/stock")
-    public String stock(Model model, @RequestParam String symbol) {
-
-        List<StockData> result = stockDataRepository.getLatestBySymbol(symbol.toUpperCase());
-        if(result.isEmpty()){
-            throw new RuntimeException("Not Found Data: "+ symbol);
-        }
-
-        StockData stock = result.get(0);
-        model.addAttribute("stock", stock);
-
-        return "stock.html";
-    }
-
-    @GetMapping("/list")
+    @GetMapping("/stock/list")
     public String stockList(Model model) {
 
-        List<Stock> result = stockRepository.findAll();
+        List<Stock> result = stockRepository.getStockList();
         model.addAttribute("stocks", result);
 
         return "list.html";
@@ -49,21 +31,11 @@ public class StockController {
     // PostMapping -----------------------------------------
 
     @PostMapping("/stock")
-    public String searchStock(Model model, String symbol) {
+    public String stock(Model model, @RequestParam String symbol) {
 
-        var result = stockRepository.findBySymbol(symbol);
-        Stock stock = result.get();
+        Stock stock = stockService.getLatestStockBySymbol(symbol);
         model.addAttribute("stock", stock);
 
-        return "redirect:/list";
-    }
-
-    @PostMapping("/stock/add")
-    public String addStock(@RequestParam String symbol) {
-        Stock stock = new Stock();
-        stock.setSymbol(symbol);
-        stockRepository.save(stock);
-
-        return "redirect:/list";
+        return "stock.html";
     }
 }
--- src/main/java/com/example/stonks/stock/StockRepository.java
+++ src/main/java/com/example/stonks/stock/StockRepository.java
@@ -2,8 +2,10 @@ package com.example.stonks.stock;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,5 +13,11 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
 
     Optional<Stock> findBySymbol(String symbol);
 
+    // 가장 최근 데이터를 가져오는 함수
+    @Query("select s from Stock s where s.symbol = :symbol order by s.date desc")
+    List<Stock> getLatestBySymbol(@Param("symbol") String symbol);
 
+    // 심볼별로 가장 최근 데이터를 가져오는 쿼리
+    @Query("SELECT s FROM Stock s WHERE s.date = (SELECT MAX(subS.date) FROM Stock subS WHERE subS.symbol = s.symbol)")
+    List<Stock> getStockList();
 }
