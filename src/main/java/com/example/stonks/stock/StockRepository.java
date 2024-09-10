d4b9478d82e20a087f3fd8d16a69cf239781ba15
bhoon <leafloveu@naver.com>
2024-09-10 22:13:02 +0900

    FEAT: StockData 테이블 삭제 Stock 테이블에는 symbol만 삽입 StockDto를 이용해 csv에서 Data 가져옴

--- src/main/java/com/example/stonks/stock/StockRepository.java
+++ src/main/java/com/example/stonks/stock/StockRepository.java
@@ -1,23 +1,9 @@
 package com.example.stonks.stock;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
-    Optional<Stock> findBySymbol(String symbol);
-
-    // 가장 최근 데이터를 가져오는 함수
-    @Query("select s from Stock s where s.symbol = :symbol order by s.date desc")
-    List<Stock> getLatestBySymbol(@Param("symbol") String symbol);
-
-    // 심볼별로 가장 최근 데이터를 가져오는 쿼리
-    @Query("SELECT s FROM Stock s WHERE s.date = (SELECT MAX(subS.date) FROM Stock subS WHERE subS.symbol = s.symbol)")
-    List<Stock> getStockList();
 }
